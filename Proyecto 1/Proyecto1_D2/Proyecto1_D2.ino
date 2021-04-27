7263cc4ddadc36a287ce1af3ab60ac04618c7548
camilalemus <lem18272@uvg.edu.gt>
2021-04-27 17:21:04 -0600

    Dinosaurios animacion

--- Proyecto 1/Proyecto1_D2/Proyecto1_D2.ino
+++ Proyecto 1/Proyecto1_D2/Proyecto1_D2.ino
@@ -58,65 +58,83 @@ void setup() {
   LCD_Init();
   LCD_Clear(0x00);
   
-  FillRect(0, 0, 319, 206, 0x421b);
-  String text1 = "Super Mario World!";
-  LCD_Print(text1, 20, 100, 2, 0xffff, 0x421b);
+  //FillRect(0, 0, 319, 239, 0x421b);
+  FillRect(0, 0, 319, 223, 0xffff);
+  String text1 = "Dino B-day Party!";
+  LCD_Print(text1, 20, 100, 2, 0x0000, 0xffff);
 //LCD_Sprite(int x, int y, int width, int height, unsigned char bitmap[],int columns, int index, char flip, char offset);
     
   //LCD_Bitmap(unsigned int x, unsigned int y, unsigned int width, unsigned int height, unsigned char bitmap[]);
   
-  
-  for(int x = 0; x <319; x++){
-    LCD_Bitmap(x, 52, 16, 16, tile2);
-    LCD_Bitmap(x, 68, 16, 16, tile);
-    
-    LCD_Bitmap(x, 207, 16, 16, tile);
-    LCD_Bitmap(x, 223, 16, 16, tile);
+  for(int x = 0; x<340; x++){
+    LCD_Bitmap(x,224, 16, 16, grama);
     x += 15;
- }
+  }
+
+
+  
+//  for(int x = 0; x <319; x++){
+//    LCD_Bitmap(x, 52, 16, 16, tile2);
+//    LCD_Bitmap(x, 68, 16, 16, tile);
+//    
+//    LCD_Bitmap(x, 207, 16, 16, tile);
+//    LCD_Bitmap(x, 223, 16, 16, tile);
+//    x += 15;
+// }
   
 }
 //***************************************************************************************************************************************
 // Loop Infinito
 //***************************************************************************************************************************************
 void loop() {
-  for(int x = 0; x <320-32; x++){
-    delay(15);
-    int anim2 = (x/35)%2;
-    
-    LCD_Sprite(x,100,16,24,planta,2,anim2,0,1);
-    V_line( x -1, 100, 24, 0x421b);
-    
-    //LCD_Bitmap(x, 100, 32, 32, prueba);
-    
-    int anim = (x/11)%8;
-    
 
-    int anim3 = (x/11)%4;
-    
-    LCD_Sprite(x, 20, 16, 32, mario,8, anim,1, 0);
-    V_line( x -1, 20, 32, 0x421b);
- 
-    //LCD_Sprite(x,100,32,32,bowser,4,anim3,0,1);
-    //V_line( x -1, 100, 32, 0x421b);
- 
- 
-    LCD_Sprite(x, 140, 16, 16, enemy,2, anim2,1, 0);
-    V_line( x -1, 140, 16, 0x421b);
+  for (int i = 0; i<320-31; i++){
+      int anim = (i/35)%2;
+  LCD_Sprite(0, 180, 31, 42, dino, 2 , anim, 0, 0);
+  };
   
-    LCD_Sprite(x, 175, 16, 32, luigi,8, anim,1, 0);
-    V_line( x -1, 175, 32, 0x421b);
-
-    LCD_Sprite(144, 150, 16, 20, pastel, 2, anim, 1,0);
-    V_line( x -1, 175, 32, 0x421b);
-  }
-  for(int x = 320-32; x >0; x--){
-    delay(5);
-    int anim = (x/11)%8;
-    int anim2 = (x/11)%2;
-    
-    LCD_Sprite(x,100,16,24,planta,2,anim2,0,0);
-    V_line( x + 16, 100, 24, 0x421b);
+  for (int i = 0; i<320-31; i++){
+      int anim = (i/35)%2;
+  LCD_Sprite(288, 180, 31, 42, dino, 2 , anim, 1, 0);
+  };
+  
+//  for(int x = 0; x <320-32; x++){
+//    delay(15);
+//    int anim2 = (x/35)%2;
+//    
+//    LCD_Sprite(x,100,16,24,planta,2,anim2,0,1);
+//    V_line( x -1, 100, 24, 0x421b);
+//    
+//    //LCD_Bitmap(x, 100, 32, 32, prueba);
+//    
+//    int anim = (x/11)%8;
+//    
+//
+//    int anim3 = (x/11)%4;
+//    
+//    LCD_Sprite(x, 20, 16, 32, mario,8, anim,1, 0);
+//    V_line( x -1, 20, 32, 0x421b);
+// 
+//    //LCD_Sprite(x,100,32,32,bowser,4,anim3,0,1);
+//    //V_line( x -1, 100, 32, 0x421b);
+// 
+// 
+//    LCD_Sprite(x, 140, 16, 16, enemy,2, anim2,1, 0);
+//    V_line( x -1, 140, 16, 0x421b);
+//  
+//    LCD_Sprite(x, 175, 16, 32, luigi,8, anim,1, 0);
+//    V_line( x -1, 175, 32, 0x421b);
+//
+//    LCD_Sprite(144, 150, 16, 20, pastel, 2, anim, 1,0);
+//    V_line( x -1, 175, 32, 0x421b);
+//  }
+//  for(int x = 320-32; x >0; x--){
+//    delay(5);
+//    int anim = (x/11)%8;
+//    int anim2 = (x/11)%2;
+//    
+//    LCD_Sprite(x,100,16,24,planta,2,anim2,0,0);
+//    V_line( x + 16, 100, 24, 0x421b);
     
     //LCD_Bitmap(x, 100, 32, 32, prueba);
     
@@ -128,7 +146,8 @@ void loop() {
 
     //LCD_Sprite(x, 20, 16, 32, mario,8, anim,0, 0);
     //V_line( x + 16, 20, 32, 0x421b);
-  } 
+//  } 
+
 
 }
 //***************************************************************************************************************************************
